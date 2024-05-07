d7ae1298ea1afce706390b84031b09490dd39dae
Chris Fryer <cfryerdev@gmail.com>
2024-05-07 16:00:57 -0600

    format change

--- cmd/console/commands.go
+++ cmd/console/commands.go
@@ -26,30 +26,30 @@ func HandleCommands(cmd string, db *gorm.DB) {
 		var entities []database.SampleEntity
 		db.Find(&entities)
 		for _, entity := range entities {
-			fmt.Printf("ID: %d, Name: %s, Description: %s\n", entity.ID, entity.Name, entity.Description)
+			fmt.Printf("  ID: %d, Name: %s, Description: %s\n", entity.ID, entity.Name, entity.Description)
 		}
 	case "read":
 		id := argMap["id"]
 		var entity database.SampleEntity
 		db.First(&entity, id)
-		fmt.Printf("ID: %d, Name: %s, Description: %s\n", entity.ID, entity.Name, entity.Description)
+		fmt.Printf("  ID: %d, Name: %s, Description: %s\n", entity.ID, entity.Name, entity.Description)
 	case "delete":
 		id := argMap["id"]
 		db.Delete(&database.SampleEntity{}, id)
-		fmt.Println("Deleted entity with ID:", id)
+		fmt.Println("  Deleted entity with ID:", id)
 	case "update":
 		id := argMap["id"]
 		name := argMap["name"]
 		description := argMap["description"]
 		db.Model(&database.SampleEntity{}).Where("id = ?", id).Updates(database.SampleEntity{Name: name, Description: description})
-		fmt.Println("Updated entity with ID:", id)
+		fmt.Println("  Updated entity with ID:", id)
 	case "insert":
 		name := argMap["name"]
 		description := argMap["description"]
 		entity := database.SampleEntity{Name: name, Description: description}
 		db.Create(&entity)
-		fmt.Println("Inserted new entity with ID:", entity.ID)
+		fmt.Println("  Inserted new entity with ID:", entity.ID)
 	default:
-		fmt.Println("Unknown command")
+		fmt.Println("  Unknown command.")
 	}
 }
